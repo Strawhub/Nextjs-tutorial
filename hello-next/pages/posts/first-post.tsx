7bd6a573568e747acdbc909125b16e282e66980d
Strawhub <k7to.so23.ten717@i.softbank.jp>
2022-11-13 23:43:34 +0900

    change head  comment

--- hello-next/pages/posts/first-post.tsx
+++ hello-next/pages/posts/first-post.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/Home.module.css'
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1>1st Post</h1>
       <h2 className={styles.title}>
         <Link href="/">
           <div>Back to home</div>
